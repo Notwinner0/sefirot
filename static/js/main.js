13d821085f24978ae1985ec19c1c2c0053373320
Notwinner <iliazukov61@gmail.com>
2025-03-30 11:31:52 +0300

    switching game states

--- static/js/main.js
+++ static/js/main.js
@@ -1,47 +1,65 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var globalGameState = 0
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+function initGameClicker() {
+    var clickerScore = 0; // Используем let вместо var для лучшего контроля области видимости
+    const container = document.querySelector('.container');
+    const helloWorld = document.querySelector('.hello');
+    const score = document.createElement('div');
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    score.classList.add('score');
 
-camera.position.z = 5;
+    function clickerGame() {
+        clickerScore += 1;
+        score.innerText = clickerScore; // Обновляем текст score внутри функции
+    }
 
-let speen = -0.001
-
-function animate() {
-
-    cube.rotation.x += speen
-    cube.scale.z += speen / 5
-    speen += 0.00001
-
-    cube.rotation.y += 0.05;
-
-    renderer.render(scene, camera);
-
+    score.innerText = clickerScore; // Инициализируем текст score при загрузке страницы
+    helloWorld.addEventListener('click', clickerGame); // Передаем функцию clickerGame без вызова
+    container.appendChild(score);
 }
 
-let globalScore = 0; // Используем let вместо var для лучшего контроля области видимости
-const container = document.querySelector('.container');
-const helloWorld = document.querySelector('.hello');
-const score = document.createElement('div');
-score.classList.add('score');
-
-function clickerGame() {
-    globalScore += 1;
-    score.innerText = globalScore; // Обновляем текст score внутри функции
+function offline() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
+    
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    
+    camera.position.z = 5;
+    
+    let speen = -0.001
+    
+    function animate() {
+    
+        cube.rotation.x += speen
+        cube.scale.z += speen / 5
+        speen += 0.00001
+    
+        cube.rotation.y += 0.05;
+    
+        renderer.render(scene, camera);
+    
+    }
 }
 
-score.innerText = globalScore; // Инициализируем текст score при загрузке страницы
-helloWorld.addEventListener('click', clickerGame); // Передаем функцию clickerGame без вызова
-container.appendChild(score);
+
+switch (globalGameState) {
+    case 0:
+        initGameClicker();
+        break;
+
+    default:
+        offline()
+        break;
+}
